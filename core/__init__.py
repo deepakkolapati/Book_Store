292e6cb652a33868deee8e8c96c509b35c326f52
deepakkolapati <deepakchandu9@gmail.com>
2024-03-20 15:25:59 +0530

    [deepak] Added mail

--- core/__init__.py
+++ core/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from flask_mail import Mail
+from settings import settings
+
+mail=Mail()
 
 def create_app(database):
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql+psycopg2://postgres:9033@localhost:5432/{database}"
+    app.config['SQLALCHEMY_DATABASE_URI'] = settings.database_url.format(database=database)
     app.config['SQLALCHEMY_TRACK_MODIFICATION'] = True
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = settings.mail_port
+    app.config['MAIL_USERNAME'] = settings.sender
+    app.config['MAIL_PASSWORD'] = settings.mail_password
+    app.config['MAIL_USE_TLS'] = False
+    app.config['MAIL_USE_SSL'] = True
+    mail.init_app(app)
     return app
 
 
