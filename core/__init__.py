a1ad8d76e61af9d3967fe408c36d6cb8afe6838f
deepakkolapati <deepakchandu9@gmail.com>
2024-03-30 12:01:24 +0530

    [deepak] Added celery

--- core/__init__.py
+++ core/__init__.py
@@ -15,8 +15,19 @@ def create_app(database , mode='debug'):
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
         app.config['SQLALCHEMY_TRACK_MODIFICATION'] = True
         app.config['TESTING'] = True
-
-
+    app.config.from_mapping(
+    CELERY=dict(
+        broker_url="redis://127.0.0.1:6379/0",
+        result_backend="redis://127.0.0.1:6379/0",
+        broker_connection_retry_on_startup=True,
+        # task_ignore_result=True,
+        redbeat_redis_url = "redis://localhost:6379/0",
+        redbeat_lock_key = None,
+        enable_utc=True,
+        beat_max_loop_interval=5,
+        beat_scheduler='redbeat.schedulers.RedBeatScheduler'
+                 ),
+        )
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = settings.mail_port
     app.config['MAIL_USERNAME'] = settings.sender
